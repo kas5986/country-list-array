6d6ab7231349861ffa45a5f9bcf43cfe4e31c3e3
kas5986 <shafiq_shaheen@hotmail.com>
2014-07-02 08:36:37 -0700

    counties.php
    associative array

---
+++ countries.php
@@ -0,0 +1,346 @@
+$countries = array( 
+  array( "name" => "Albania", 
+       "latitude" => "41.153332",
+       "longitude" => "20.168331" 
+     ),
+  array("name" => "Argentina", 
+       "latitude" => "-38.416097",
+       "longitude" => "-63.616672" 
+     ),
+  array("name" => "Armenia", 
+       "latitude" => "40.069099",
+       "longitude" => "45.038189" 
+     ),
+  array("name" => "Australia", 
+       "latitude" => "-25.274398",
+       "longitude" => "133.775136" 
+     ),
+  array("name" => "Austria", 
+       "latitude" => "47.516231",
+       "longitude" => "14.550072" 
+     ),
+  array("name" => "Azerbaijan", 
+       "latitude" => "40.143105",
+       "longitude" => "47.576927" 
+     ),
+  array("name" => "Bahrain", 
+       "latitude" => "25.930414",
+       "longitude" => "50.637772" 
+     ),
+  array("name" => "Barbados", 
+       "latitude" => "13.193887",
+       "longitude" => "-59.543198" 
+     ),
+  array("name" => "Belgium", 
+       "latitude" => "50.503887",
+       "longitude" => "4.469936" 
+     ),
+  array("name" => "Bermuda", 
+       "latitude" => "32.321384",
+       "longitude" => "-64.75737" 
+     ),
+  array("name" => "Bolivia", 
+       "latitude" => "-16.290154",
+       "longitude" => "-63.588653" 
+     ),
+  array("name" => "Brazil", 
+       "latitude" => "-14.235004",
+       "longitude" => "-51.92528" 
+     ),
+  array("name" => "Bulgaria", 
+       "latitude" => "42.733883",
+       "longitude" => "25.48583" 
+     ),
+  array("name" => "Canada", 
+       "latitude" => "56.130366",
+       "longitude" => "-106.346771" 
+     ),
+  array("name" => "Chile", 
+       "latitude" => "-35.675147",
+       "longitude" => "-71.542969" 
+     ),
+  array("name" => "China", 
+       "latitude" => "35.86166",
+       "longitude" => "104.195397" 
+     ),
+  array("name" => "Colombia", 
+       "latitude" => "4.570868",
+       "longitude" => "-74.297333" 
+     ),
+  array("name" => "Costa Rica", 
+       "latitude" => "9.748917",
+       "longitude" => "-83.753428" 
+     ),
+  array("name" => "Croatia", 
+       "latitude" => "45.1",
+       "longitude" => "15.2" 
+     ),
+  array("name" => "Cuba", 
+       "latitude" => "21.521757",
+       "longitude" => "-77.781167" 
+     ),
+  array("name" => "Cyprus", 
+       "latitude" => "35.126413",
+       "longitude" => "33.429859" 
+     ),
+  array("name" => "Czech Republic", 
+       "latitude" => "49.817492",
+       "longitude" => "15.472962" 
+     ),
+  array("name" => "Dominican Republic", 
+       "latitude" => "18.735693",
+       "longitude" => "-70.162651" 
+     ),
+  array("name" => "Ecuador", 
+       "latitude" => "-1.831239",
+       "longitude" => "-78.183406" 
+     ),
+  array("name" => "Egypt", 
+       "latitude" => "26.820553",
+       "longitude" => "30.802498" 
+     ),
+  array("name" => "Ethiopia", 
+       "latitude" => "9.145",
+       "longitude" => "40.489673" 
+     ),
+  array("name" => "Fiji", 
+       "latitude" => "-16.578193",
+       "longitude" => "179.414413" 
+     ),
+  array("name" => "France", 
+       "latitude" => "46.227638",
+       "longitude" => "2.213749" 
+     ),
+  array("name" => "Georgia", 
+       "latitude" => "42.315407",
+       "longitude" => "43.356892" 
+     ),
+  array("name" => "Germany", 
+       "latitude" => "51.165691",
+       "longitude" => "10.451526" 
+     ),
+  array("name" => "Greece", 
+       "latitude" => "39.074208",
+       "longitude" => "21.824312" 
+     ),
+  array("name" => "Guatemala", 
+       "latitude" => "15.783471",
+       "longitude" => "-90.230759" 
+     ),
+  array("name" => "Hungary", 
+       "latitude" => "47.162494",
+       "longitude" => "19.503304" 
+     ),
+  array("name" => "India", 
+       "latitude" => "20.593684",
+       "longitude" => "78.96288" 
+     ),
+  array("name" => "Indonesia", 
+       "latitude" => "-0.789275",
+       "longitude" => "113.921327" 
+     ),
+  array("name" => "Iraq", 
+       "latitude" => "33.223191",
+       "longitude" => "43.679291" 
+     ),
+  array("name" => "Ireland", 
+       "latitude" => "53.41291",
+       "longitude" => "-8.24389" 
+     ),
+  array("name" => "Israel", 
+       "latitude" => "31.046051",
+       "longitude" => "34.851612" 
+     ),
+  array("name" => "Italy", 
+       "latitude" => "41.87194",
+       "longitude" => "12.56738" 
+     ),
+  array("name" => "Jordan", 
+       "latitude" => "30.585164",
+       "longitude" => "36.238414" 
+     ),
+  array("name" => "Kenya", 
+       "latitude" => "-0.023559",
+       "longitude" => "37.906193" 
+     ),
+  array("name" => "Kuwait", 
+       "latitude" => "29.31166",
+       "longitude" => "47.481766" 
+     ),
+  array("name" => "Latvia", 
+       "latitude" => "56.879635",
+       "longitude" => "24.603189" 
+     ),
+  array("name" => "Lebanon", 
+       "latitude" => "33.854721",
+       "longitude" => "35.862285" 
+     ),
+  array("name" => "Lithuania", 
+       "latitude" => "55.169438",
+       "longitude" => "23.881275" 
+     ),
+  array("name" => "Macedonia", 
+       "latitude" => "41.608635",
+       "longitude" => "21.745275" 
+     ),
+  array("name" => "Malaysia", 
+       "latitude" => "4.210484",
+       "longitude" => "101.975766" 
+     ),
+  array("name" => "Malta", 
+       "latitude" => "35.937496",
+       "longitude" => "14.375416" 
+     ),
+  array("name" => "Mexico", 
+       "latitude" => "23.634501",
+       "longitude" => "-102.552784" 
+     ),
+  array("name" => "Moldova", 
+       "latitude" => "47.411631",
+       "longitude" => "28.369885" 
+     ),
+  array("name" => "Morocco", 
+       "latitude" => "31.791702",
+       "longitude" => "-7.09262" 
+     ),
+  array("name" => "New Zealand", 
+       "latitude" => "-40.900557",
+       "longitude" => "174.885971" 
+     ),
+  array("name" => "Nigeria", 
+       "latitude" => "9.081999",
+       "longitude" => "8.675277" 
+     ),
+  array("name" => "Norway", 
+       "latitude" => "60.472024",
+       "longitude" => "8.468946" 
+     ),
+  array("name" => "Oman", 
+       "latitude" => "21.512583",
+       "longitude" => "55.923255" 
+     ),	
+  array("name" => "Pakistan", 
+       "latitude" => "30.375321",
+       "longitude" => "69.345116" 
+     ),	
+  array("name" => "Panama", 
+       "latitude" => "8.537981",
+       "longitude" => "-80.782127" 
+     ),	
+  array("name" => "Peru", 
+       "latitude" => "-9.189967",
+       "longitude" => "-75.015152" 
+     ),	
+  array("name" => "Philippines", 
+       "latitude" => "12.879721",
+       "longitude" => "121.774017" 
+     ),	
+  array("name" => "Poland", 
+       "latitude" => "51.919438",
+       "longitude" => "19.145136" 
+     ),	
+  array("name" => "Portugal", 
+       "latitude" => "39.399872",
+       "longitude" => "-8.224454" 
+     ),	
+  array("name" => "Qatar", 
+       "latitude" => "25.354826",
+       "longitude" => "51.183884" 
+     ),
+  array("name" => "Mauritius", 
+       "latitude" => "-20.348404",
+       "longitude" => "57.552152" 
+     ),	
+  array("name" => "Romania", 
+       "latitude" => "45.943161",
+       "longitude" => "24.96676" 
+     ),	
+  array("name" => "Russia", 
+       "latitude" => "61.52401",
+       "longitude" => "105.318756" 
+     ),	
+  array("name" => "Saudi Arabia", 
+       "latitude" => "23.885942",
+       "longitude" => "45.079162" 
+     ),	
+  array("name" => "Serbia", 
+       "latitude" => "44.016521",
+       "longitude" => "21.005859" 
+     ),	
+  array("name" => "Singapore", 
+       "latitude" => "1.352083",
+       "longitude" => "103.819836" 
+     ),		
+  array("name" => "Slovakia", 
+       "latitude" => "48.669026",
+       "longitude" => "19.699024" 
+     ),		
+  array("name" => "South Africa", 
+       "latitude" => "-30.559482",
+       "longitude" => "22.937506" 
+     ),		
+  array("name" => "South Korea", 
+       "latitude" => "35.907757",
+       "longitude" => "127.766922" 
+     ),		
+  array("name" => "Spain", 
+       "latitude" => "40.463667",
+       "longitude" => "-3.74922" 
+     ),		
+  array("name" => "Sri Lanka", 
+       "latitude" => "7.873054",
+       "longitude" => "80.771797" 
+     ),		
+  array("name" => "Swaziland", 
+       "latitude" => "-26.522503",
+       "longitude" => "31.465866" 
+     ),			
+  array("name" => "Taiwan", 
+       "latitude" => "23.69781",
+       "longitude" => "120.960515" 
+     ),			
+  array("name" => "Thailand", 
+       "latitude" => "15.870032",
+       "longitude" => "100.992541" 
+     ),			
+  array("name" => "Tunisia", 
+       "latitude" => "33.886917",
+       "longitude" => "9.537499" 
+     ),			
+  array("name" => "Turkey", 
+       "latitude" => "38.963745",
+       "longitude" => "35.243322" 
+     ),			
+  array("name" => "United Arab Emirates", 
+       "latitude" => "23.424076",
+       "longitude" => "53.847818" 
+     ),				
+  array("name" => "Ukraine", 
+       "latitude" => "48.379433",
+       "longitude" => "31.16558" 
+     ),				
+  array("name" => "United Kingdom", 
+       "latitude" => "55.378051",
+       "longitude" => "-3.435973" 
+     ),				
+  array("name" => "United States", 
+       "latitude" => "37.09024",
+       "longitude" => "-95.712891" 
+     ),				
+  array("name" => "Uruguay", 
+       "latitude" => "-32.522779",
+       "longitude" => "-55.765835" 
+     ),				
+  array("name" => "Uzbekistan", 
+       "latitude" => "41.377491",
+       "longitude" => "64.585262" 
+     ),					
+  array("name" => "Venezuela", 
+       "latitude" => "6.42375",
+       "longitude" => "-66.58973" 
+     ),					
+  array("name" => "Vietnam", 
+       "latitude" => "14.058324",
+       "longitude" => "108.277199" 
+     ),			
+);
