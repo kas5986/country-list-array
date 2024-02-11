d17baf94001f0ded0632fe71b71c4f81138fe393
Syed Muhammad Shafiq <shafiq_shaheen@hotmail.com>
2024-02-12 03:46:55 +0500

    Update countries.php
    added country ISO and phone code

--- countries.php
+++ countries.php
@@ -3,347 +3,519 @@
 $countries = array( 
   array( "name" => "Albania", 
        "latitude" => "41.153332",
-       "longitude" => "20.168331" 
+       "longitude" => "20.168331", 
+	"iso_code" => "AL",
+	"phone_code" => "355"
      ),
   array("name" => "Argentina", 
        "latitude" => "-38.416097",
-       "longitude" => "-63.616672" 
+       "longitude" => "-63.616672", 
+	"iso_code" => "AR",
+	"phone_code" => "54"
      ),
   array("name" => "Armenia", 
        "latitude" => "40.069099",
-       "longitude" => "45.038189" 
+       "longitude" => "45.038189", 
+	"iso_code" => "AM",
+	"phone_code" => "374"
      ),
   array("name" => "Australia", 
        "latitude" => "-25.274398",
-       "longitude" => "133.775136" 
+       "longitude" => "133.775136", 
+	"iso_code" => "AU",
+	"phone_code" => "61" 
      ),
   array("name" => "Austria", 
        "latitude" => "47.516231",
-       "longitude" => "14.550072" 
+       "longitude" => "14.550072", 
+	"iso_code" => "AT",
+	"phone_code" => "43" 
      ),
   array("name" => "Azerbaijan", 
        "latitude" => "40.143105",
-       "longitude" => "47.576927" 
+       "longitude" => "47.576927", 
+	"iso_code" => "AZ",
+	"phone_code" => "994" 
      ),
   array("name" => "Bahrain", 
        "latitude" => "25.930414",
-       "longitude" => "50.637772" 
+       "longitude" => "50.637772", 
+	"iso_code" => "BH",
+	"phone_code" => "973" 
      ),
   array("name" => "Barbados", 
        "latitude" => "13.193887",
-       "longitude" => "-59.543198" 
+       "longitude" => "-59.543198", 
+	"iso_code" => "BB",
+	"phone_code" => "1-246"  
      ),
   array("name" => "Belgium", 
        "latitude" => "50.503887",
-       "longitude" => "4.469936" 
+       "longitude" => "4.469936", 
+	"iso_code" => "BE",
+	"phone_code" => "32" 
      ),
   array("name" => "Bermuda", 
        "latitude" => "32.321384",
-       "longitude" => "-64.75737" 
+       "longitude" => "-64.75737", 
+	"iso_code" => "BM",
+	"phone_code" => "1-441"  
      ),
   array("name" => "Bolivia", 
        "latitude" => "-16.290154",
-       "longitude" => "-63.588653" 
+       "longitude" => "-63.588653", 
+	"iso_code" => "BO",
+	"phone_code" => "591" 
      ),
   array("name" => "Brazil", 
        "latitude" => "-14.235004",
-       "longitude" => "-51.92528" 
+       "longitude" => "-51.92528", 
+	"iso_code" => "BR",
+	"phone_code" => "55"
      ),
   array("name" => "Bulgaria", 
        "latitude" => "42.733883",
-       "longitude" => "25.48583" 
+       "longitude" => "25.48583", 
+	"iso_code" => "BG",
+	"phone_code" => "359" 
      ),
   array("name" => "Canada", 
        "latitude" => "56.130366",
-       "longitude" => "-106.346771" 
+       "longitude" => "-106.346771", 
+	"iso_code" => "CA",
+	"phone_code" => "1"  
      ),
   array("name" => "Chile", 
        "latitude" => "-35.675147",
-       "longitude" => "-71.542969" 
+       "longitude" => "-71.542969", 
+	"iso_code" => "CL",
+	"phone_code" => "56"  
      ),
   array("name" => "China", 
        "latitude" => "35.86166",
-       "longitude" => "104.195397" 
+       "longitude" => "104.195397", 
+	"iso_code" => "CN",
+	"phone_code" => "86" 
      ),
   array("name" => "Colombia", 
        "latitude" => "4.570868",
-       "longitude" => "-74.297333" 
+       "longitude" => "-74.297333", 
+	"iso_code" => "CO",
+	"phone_code" => "57"  
      ),
   array("name" => "Costa Rica", 
        "latitude" => "9.748917",
-       "longitude" => "-83.753428" 
+       "longitude" => "-83.753428", 
+	"iso_code" => "CR",
+	"phone_code" => "506"
      ),
   array("name" => "Croatia", 
        "latitude" => "45.1",
-       "longitude" => "15.2" 
+       "longitude" => "15.2", 
+	"iso_code" => "HR",
+	"phone_code" => "385"
      ),
   array("name" => "Cuba", 
        "latitude" => "21.521757",
-       "longitude" => "-77.781167" 
+       "longitude" => "-77.781167", 
+	"iso_code" => "CU",
+	"phone_code" => "53"
      ),
   array("name" => "Cyprus", 
        "latitude" => "35.126413",
-       "longitude" => "33.429859" 
+       "longitude" => "33.429859", 
+	"iso_code" => "CY",
+	"phone_code" => "357"
      ),
   array("name" => "Czech Republic", 
        "latitude" => "49.817492",
-       "longitude" => "15.472962" 
+       "longitude" => "15.472962", 
+	"iso_code" => "CZ",
+	"phone_code" => "420" 
      ),
   array("name" => "Dominican Republic", 
        "latitude" => "18.735693",
-       "longitude" => "-70.162651" 
+       "longitude" => "-70.162651", 
+	"iso_code" => "DO",
+	"phone_code" => "1-809,1-829,1-849"
      ),
   array("name" => "Ecuador", 
        "latitude" => "-1.831239",
-       "longitude" => "-78.183406" 
+       "longitude" => "-78.183406", 
+	"iso_code" => "EC",
+	"phone_code" => "593"  
      ),
   array("name" => "Egypt", 
        "latitude" => "26.820553",
-       "longitude" => "30.802498" 
+       "longitude" => "30.802498", 
+	"iso_code" => "EG",
+	"phone_code" => "20" 
      ),
   array("name" => "Ethiopia", 
        "latitude" => "9.145",
-       "longitude" => "40.489673" 
+       "longitude" => "40.489673", 
+	"iso_code" => "ET",
+	"phone_code" => "251"  
      ),
   array("name" => "Fiji", 
        "latitude" => "-16.578193",
-       "longitude" => "179.414413" 
+       "longitude" => "179.414413", 
+	"iso_code" => "FJ",
+	"phone_code" => "679" 
      ),
   array("name" => "France", 
        "latitude" => "46.227638",
-       "longitude" => "2.213749" 
+       "longitude" => "2.213749", 
+	"iso_code" => "FR",
+	"phone_code" => "33"
      ),
   array("name" => "Georgia", 
        "latitude" => "42.315407",
-       "longitude" => "43.356892" 
+       "longitude" => "43.356892", 
+	"iso_code" => "GE",
+	"phone_code" => "995" 
      ),
   array("name" => "Germany", 
        "latitude" => "51.165691",
-       "longitude" => "10.451526" 
+       "longitude" => "10.451526", 
+	"iso_code" => "DE",
+	"phone_code" => "49" 
      ),
   array("name" => "Greece", 
        "latitude" => "39.074208",
-       "longitude" => "21.824312" 
+       "longitude" => "21.824312", 
+	"iso_code" => "GR",
+	"phone_code" => "30" 
      ),
   array("name" => "Guatemala", 
        "latitude" => "15.783471",
-       "longitude" => "-90.230759" 
+       "longitude" => "-90.230759", 
+	"iso_code" => "GT",
+	"phone_code" => "502" 
      ),
   array("name" => "Hungary", 
        "latitude" => "47.162494",
-       "longitude" => "19.503304" 
+       "longitude" => "19.503304", 
+	"iso_code" => "HU",
+	"phone_code" => "36" 
      ),
   array("name" => "India", 
        "latitude" => "20.593684",
-       "longitude" => "78.96288" 
+       "longitude" => "78.96288", 
+	"iso_code" => "IN",
+	"phone_code" => "91" 
      ),
   array("name" => "Indonesia", 
        "latitude" => "-0.789275",
-       "longitude" => "113.921327" 
+       "longitude" => "113.921327", 
+	"iso_code" => "ID",
+	"phone_code" => "62" 
      ),
   array("name" => "Iraq", 
        "latitude" => "33.223191",
-       "longitude" => "43.679291" 
+       "longitude" => "43.679291", 
+	"iso_code" => "IQ",
+	"phone_code" => "964"  
      ),
   array("name" => "Ireland", 
        "latitude" => "53.41291",
-       "longitude" => "-8.24389" 
+       "longitude" => "-8.24389", 
+	"iso_code" => "IE",
+	"phone_code" => "353" 
      ),
   array("name" => "Israel", 
        "latitude" => "31.046051",
-       "longitude" => "34.851612" 
+       "longitude" => "34.851612", 
+	"iso_code" => "IL",
+	"phone_code" => "972" 
      ),
   array("name" => "Italy", 
        "latitude" => "41.87194",
-       "longitude" => "12.56738" 
+       "longitude" => "12.56738", 
+	"iso_code" => "IT",
+	"phone_code" => "39" 
      ),
   array("name" => "Jordan", 
        "latitude" => "30.585164",
-       "longitude" => "36.238414" 
+       "longitude" => "36.238414", 
+	"iso_code" => "JO",
+	"phone_code" => "962" 
      ),
   array("name" => "Kenya", 
        "latitude" => "-0.023559",
-       "longitude" => "37.906193" 
+       "longitude" => "37.906193", 
+	"iso_code" => "KE",
+	"phone_code" => "254"
      ),
   array("name" => "Kuwait", 
        "latitude" => "29.31166",
-       "longitude" => "47.481766" 
+       "longitude" => "47.481766", 
+	"iso_code" => "KW",
+	"phone_code" => "965" 
      ),
   array("name" => "Latvia", 
        "latitude" => "56.879635",
-       "longitude" => "24.603189" 
+       "longitude" => "24.603189", 
+	"iso_code" => "LV",
+	"phone_code" => "371" 
      ),
   array("name" => "Lebanon", 
        "latitude" => "33.854721",
-       "longitude" => "35.862285" 
+       "longitude" => "35.862285", 
+	"iso_code" => "LB",
+	"phone_code" => "961" 
      ),
   array("name" => "Lithuania", 
        "latitude" => "55.169438",
-       "longitude" => "23.881275" 
+       "longitude" => "23.881275", 
+	"iso_code" => "LT",
+	"phone_code" => "370" 
      ),
   array("name" => "Macedonia", 
        "latitude" => "41.608635",
-       "longitude" => "21.745275" 
+       "longitude" => "21.745275", 
+	"iso_code" => "MK",
+	"phone_code" => "389" 
      ),
   array("name" => "Malaysia", 
        "latitude" => "4.210484",
-       "longitude" => "101.975766" 
+       "longitude" => "101.975766", 
+	"iso_code" => "MY",
+	"phone_code" => "60"  
      ),
   array("name" => "Malta", 
        "latitude" => "35.937496",
-       "longitude" => "14.375416" 
+       "longitude" => "14.375416", 
+	"iso_code" => "MT",
+	"phone_code" => "356" 
      ),
   array("name" => "Mexico", 
        "latitude" => "23.634501",
-       "longitude" => "-102.552784" 
+       "longitude" => "-102.552784", 
+	"iso_code" => "MX",
+	"phone_code" => "52" 
      ),
   array("name" => "Moldova", 
        "latitude" => "47.411631",
-       "longitude" => "28.369885" 
+       "longitude" => "28.369885", 
+	"iso_code" => "MD",
+	"phone_code" => "373"  
      ),
   array("name" => "Morocco", 
        "latitude" => "31.791702",
-       "longitude" => "-7.09262" 
+       "longitude" => "-7.09262", 
+	"iso_code" => "MA",
+	"phone_code" => "212"  
      ),
   array("name" => "New Zealand", 
        "latitude" => "-40.900557",
-       "longitude" => "174.885971" 
+       "longitude" => "174.885971", 
+	"iso_code" => "NZ",
+	"phone_code" => "64"  
      ),
   array("name" => "Nigeria", 
        "latitude" => "9.081999",
-       "longitude" => "8.675277" 
+       "longitude" => "8.675277", 
+	"iso_code" => "NG",
+	"phone_code" => "234"   
      ),
   array("name" => "Norway", 
        "latitude" => "60.472024",
-       "longitude" => "8.468946" 
+       "longitude" => "8.468946", 
+	"iso_code" => "NO",
+	"phone_code" => "47" 
      ),
   array("name" => "Oman", 
        "latitude" => "21.512583",
-       "longitude" => "55.923255" 
+       "longitude" => "55.923255", 
+	"iso_code" => "OM",
+	"phone_code" => "968" 
      ),	
   array("name" => "Pakistan", 
        "latitude" => "30.375321",
-       "longitude" => "69.345116" 
+       "longitude" => "69.345116", 
+	"iso_code" => "PK",
+	"phone_code" => "92" 
      ),	
   array("name" => "Panama", 
        "latitude" => "8.537981",
-       "longitude" => "-80.782127" 
+       "longitude" => "-80.782127", 
+	"iso_code" => "PA",
+	"phone_code" => "507"  
      ),	
   array("name" => "Peru", 
        "latitude" => "-9.189967",
-       "longitude" => "-75.015152" 
+       "longitude" => "-75.015152", 
+	"iso_code" => "PE",
+	"phone_code" => "51"
      ),	
   array("name" => "Philippines", 
        "latitude" => "12.879721",
-       "longitude" => "121.774017" 
+       "longitude" => "121.774017", 
+	"iso_code" => "PH",
+	"phone_code" => "63" 
      ),	
   array("name" => "Poland", 
        "latitude" => "51.919438",
-       "longitude" => "19.145136" 
+       "longitude" => "19.145136", 
+	"iso_code" => "PL",
+	"phone_code" => "48" 
      ),	
   array("name" => "Portugal", 
        "latitude" => "39.399872",
-       "longitude" => "-8.224454" 
+       "longitude" => "-8.224454", 
+	"iso_code" => "PT",
+	"phone_code" => "351" 
      ),	
   array("name" => "Qatar", 
        "latitude" => "25.354826",
-       "longitude" => "51.183884" 
+       "longitude" => "51.183884", 
+	"iso_code" => "QA",
+	"phone_code" => "974"  
      ),
   array("name" => "Mauritius", 
        "latitude" => "-20.348404",
-       "longitude" => "57.552152" 
+       "longitude" => "57.552152", 
+	"iso_code" => "MU",
+	"phone_code" => "230"
      ),	
   array("name" => "Romania", 
        "latitude" => "45.943161",
-       "longitude" => "24.96676" 
+       "longitude" => "24.96676", 
+	"iso_code" => "RO",
+	"phone_code" => "40" 
      ),	
   array("name" => "Russia", 
        "latitude" => "61.52401",
-       "longitude" => "105.318756" 
+       "longitude" => "105.318756", 
+	"iso_code" => "RU",
+	"phone_code" => "7" 
      ),	
   array("name" => "Saudi Arabia", 
        "latitude" => "23.885942",
-       "longitude" => "45.079162" 
+       "longitude" => "45.079162", 
+	"iso_code" => "SA",
+	"phone_code" => "966"  
      ),	
   array("name" => "Serbia", 
        "latitude" => "44.016521",
-       "longitude" => "21.005859" 
+       "longitude" => "21.005859", 
+	"iso_code" => "RS",
+	"phone_code" => "381"  
      ),	
   array("name" => "Singapore", 
        "latitude" => "1.352083",
-       "longitude" => "103.819836" 
+       "longitude" => "103.819836", 
+	"iso_code" => "SG",
+	"phone_code" => "65"   
      ),		
   array("name" => "Slovakia", 
        "latitude" => "48.669026",
-       "longitude" => "19.699024" 
+       "longitude" => "19.699024", 
+	"iso_code" => "SK",
+	"phone_code" => "421"  
      ),		
   array("name" => "South Africa", 
        "latitude" => "-30.559482",
-       "longitude" => "22.937506" 
+       "longitude" => "22.937506", 
+	"iso_code" => "SK",
+	"phone_code" => "421" 
      ),		
   array("name" => "South Korea", 
        "latitude" => "35.907757",
-       "longitude" => "127.766922" 
+       "longitude" => "127.766922", 
+	"iso_code" => "KR",
+	"phone_code" => "82"  
      ),		
   array("name" => "Spain", 
        "latitude" => "40.463667",
-       "longitude" => "-3.74922" 
+       "longitude" => "-3.74922", 
+	"iso_code" => "ES",
+	"phone_code" => "34" 
      ),		
   array("name" => "Sri Lanka", 
        "latitude" => "7.873054",
-       "longitude" => "80.771797" 
+       "longitude" => "80.771797", 
+	"iso_code" => "LK",
+	"phone_code" => "94"
      ),		
   array("name" => "Swaziland", 
        "latitude" => "-26.522503",
-       "longitude" => "31.465866" 
+       "longitude" => "31.465866", 
+	"iso_code" => "SZ",
+	"phone_code" => "268" 
      ),			
   array("name" => "Taiwan", 
        "latitude" => "23.69781",
-       "longitude" => "120.960515" 
+       "longitude" => "120.960515", 
+	"iso_code" => "TW",
+	"phone_code" => "886"  
      ),			
   array("name" => "Thailand", 
        "latitude" => "15.870032",
-       "longitude" => "100.992541" 
+       "longitude" => "100.992541", 
+	"iso_code" => "TH",
+	"phone_code" => "66"  
      ),			
   array("name" => "Tunisia", 
        "latitude" => "33.886917",
-       "longitude" => "9.537499" 
+       "longitude" => "9.537499", 
+	"iso_code" => "TN",
+	"phone_code" => "216" 
      ),			
   array("name" => "Turkey", 
        "latitude" => "38.963745",
-       "longitude" => "35.243322" 
+       "longitude" => "35.243322", 
+	"iso_code" => "TR",
+	"phone_code" => "90" 
      ),			
   array("name" => "United Arab Emirates", 
        "latitude" => "23.424076",
-       "longitude" => "53.847818" 
+       "longitude" => "53.847818", 
+	"iso_code" => "AE",
+	"phone_code" => "971"  
      ),				
   array("name" => "Ukraine", 
        "latitude" => "48.379433",
-       "longitude" => "31.16558" 
+       "longitude" => "31.16558", 
+	"iso_code" => "UA",
+	"phone_code" => "380"  
      ),				
   array("name" => "United Kingdom", 
        "latitude" => "55.378051",
-       "longitude" => "-3.435973" 
+       "longitude" => "-3.435973", 
+	"iso_code" => "GB",
+	"phone_code" => "44" 
      ),				
   array("name" => "United States", 
        "latitude" => "37.09024",
-       "longitude" => "-95.712891" 
+       "longitude" => "-95.712891", 
+	"iso_code" => "US",
+	"phone_code" => "1"  
      ),				
   array("name" => "Uruguay", 
        "latitude" => "-32.522779",
-       "longitude" => "-55.765835" 
+       "longitude" => "-55.765835", 
+	"iso_code" => "UY",
+	"phone_code" => "598"  
      ),				
   array("name" => "Uzbekistan", 
        "latitude" => "41.377491",
-       "longitude" => "64.585262" 
+       "longitude" => "64.585262", 
+	"iso_code" => "UZ",
+	"phone_code" => "998"  
      ),					
   array("name" => "Venezuela", 
        "latitude" => "6.42375",
-       "longitude" => "-66.58973" 
+       "longitude" => "-66.58973", 
+	"iso_code" => "VE",
+	"phone_code" => "58" 
      ),					
   array("name" => "Vietnam", 
        "latitude" => "14.058324",
-       "longitude" => "108.277199" 
+       "longitude" => "108.277199", 
+	"iso_code" => "VN",
+	"phone_code" => "84"  
      ),			
 );
 
